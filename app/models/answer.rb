598641ae300978846bed9178dfe639bffcc57a44
Apivat Hanvongse <apivat.hanvongse@gmail.com>
2013-09-15 14:59:16 -0400

    Save to response to database

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,6 @@
 class Answer < ActiveRecord::Base
+	validates :answer_name, :user_id, :question_id, presence: true
 	belongs_to :question
+	belongs_to :user
 	##make sure answer table in db has a question_id col.
 end
